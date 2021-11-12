90e7bb7d02089becfe94a09d8a709b856b888b99
Madelene Campos <madelenecampos@gmail.com>
2021-11-12 15:52:37 -0500

    Ensures a return of all results from api

--- movies/views.py
+++ movies/views.py
@@ -1,7 +1,9 @@
 import json
+import math
 
 import requests
 from django.contrib import messages
+from django.core.paginator import Paginator
 from django.shortcuts import redirect, render
 from moviesiveseen.settings import OMDB
 from users.models import User
@@ -12,6 +14,19 @@ BASE_URL = "http://www.omdbapi.com/"
 URL = f"{BASE_URL}{OMDB['api_key']}"
 
 
+def main_request(query, page):
+    '''Initial request to the OMDB API'''
+    request_url = f"{URL}&s={query}&page={page}"
+    response = requests.get(request_url)
+    return response.json()
+
+
+def get_total_pages(movies):
+    """Gets total number of pages, based on the value in totalResults"""
+    total_results = int(movies['totalResults'])
+    return math.ceil(total_results / 10)
+
+
 def search(request):
     """Search the OMDB database (http://www.omdbapi.com/)."""
 
@@ -20,17 +35,23 @@ def search(request):
 
     if request.method == 'POST':
         query = request.POST.get('q')
-        request_url = f"{URL}&s={query}"
-        response = requests.get(request_url)
-        movies = response.json()
-        total_results = int(movies['totalResults'])
+        movies = main_request(query, 1)
 
+        movies_list = []
+        for page in range(1, get_total_pages(movies) + 1):
+            movies = main_request(query, page)
+            movies_list.append(movies['Search'])
+        # movies_list returns a list of lists of objects.
+        # Needed to flatten that list
+        flat_movies_list = [item for sublist in movies_list for item in sublist]
+        print(flat_movies_list)
+        print(len(flat_movies_list))
         context = {
-            'movies': movies['Search']
+            'movies': flat_movies_list,
         }
 
-        movies = request.POST.getlist('watched')
-        json_str = [m.replace("\'", "\"") for m in movies]
+        watched_movies = request.POST.getlist('watched')
+        json_str = [m.replace("\'", "\"") for m in watched_movies]
 
         user = User.objects.get(username=request.user.username)
 
